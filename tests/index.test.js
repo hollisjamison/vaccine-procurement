33f6f35e0cbb2e3cfb6cc21dfaa2d08e1a845647
Hollis Jamison <hollisjamison@gmail.com>
2021-11-16 21:45:05 -0500

    Adds pure test

--- tests/index.test.js
+++ tests/index.test.js
@@ -157,4 +157,131 @@ describe('procureVaccines', () => {
 
     expect(resolvedActiveReqs).to.deep.equal(expectedResult)
   })
+
+  it('Fulfills multiple requests if there is enough units.', () => {
+    const activeReqs = [
+      {
+        brand: 'J&J',
+        type: 'shortage',
+        units: 25,
+        clinicId: 251,
+      },
+      {
+        brand: 'J&J',
+        type: 'shortage',
+        units: 25,
+        clinicId: 259,
+      },
+    ]
+    const newReq = {
+      brand: 'J&J',
+      type: 'surplus',
+      units: 50,
+      clinicId: 253,
+    }
+
+    const resolvedActiveReqs = procureVaccines(activeReqs, newReq)
+
+    const expectedResult = []
+
+    expect(resolvedActiveReqs).to.deep.equal(expectedResult)
+  })
+
+  it('Fulfills multiple requests even if there isnt enough units.', () => {
+    const activeReqs = [
+      {
+        brand: 'J&J',
+        type: 'shortage',
+        units: 25,
+        clinicId: 251,
+      },
+      {
+        brand: 'J&J',
+        type: 'shortage',
+        units: 50,
+        clinicId: 259,
+      },
+      {
+        brand: 'Moderna',
+        type: 'shortage',
+        units: 50,
+        clinicId: 275,
+      },
+    ]
+    const newReq = {
+      brand: 'J&J',
+      type: 'surplus',
+      units: 50,
+      clinicId: 253,
+    }
+
+    const resolvedActiveReqs = procureVaccines(activeReqs, newReq)
+
+    const expectedResult = [
+      {
+        brand: 'Moderna',
+        type: 'shortage',
+        units: 50,
+        clinicId: 275,
+      },
+      {
+        brand: 'J&J',
+        type: 'shortage',
+        units: 25,
+        clinicId: 259,
+      },
+    ]
+
+    expect(resolvedActiveReqs).to.deep.equal(expectedResult)
+  })
+
+  it('Is a pure function and does not effect test data', () => {
+    const activeReqs = [
+      {
+        brand: 'J&J',
+        type: 'shortage',
+        units: 25,
+        clinicId: 251,
+      },
+      {
+        brand: 'J&J',
+        type: 'shortage',
+        units: 50,
+        clinicId: 259,
+      },
+      {
+        brand: 'Moderna',
+        type: 'shortage',
+        units: 50,
+        clinicId: 275,
+      },
+    ]
+    const newReq = {
+      brand: 'J&J',
+      type: 'surplus',
+      units: 50,
+      clinicId: 253,
+    }
+
+    const resolvedActiveReqsOne = procureVaccines(activeReqs, newReq)
+    const resolvedActiveReqsTwo = procureVaccines(activeReqs, newReq)
+
+    const expectedResult = [
+      {
+        brand: 'Moderna',
+        type: 'shortage',
+        units: 50,
+        clinicId: 275,
+      },
+      {
+        brand: 'J&J',
+        type: 'shortage',
+        units: 25,
+        clinicId: 259,
+      },
+    ]
+
+    expect(resolvedActiveReqsOne).to.deep.equal(expectedResult)
+    expect(resolvedActiveReqsTwo).to.deep.equal(expectedResult)
+  })
 })
